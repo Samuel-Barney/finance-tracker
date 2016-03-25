d340bdcf47fc27e3cb1d8be91db3ed4504a0177a
Samuel Barney <samuel13@uga.edu>
2016-03-25 04:01:52 +0000

    Spinner bug fix and more AJAX for UI

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -15,6 +15,7 @@ init_stock_lookup = function(){
   });
 
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+    hide_spinner();
     $('#stock-lookup-results').replaceWith('');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
