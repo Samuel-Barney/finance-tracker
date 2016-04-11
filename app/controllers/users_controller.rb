b08a58b097990b8db06d790462b5a8b97a0b6780
Samuel Barney <samuel13@uga.edu>
2016-04-11 18:38:11 +0000

    add destroy friendship and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
   def my_friends
     @friendships = current_user.friends
   end
-  
+
   def search
     @users = User.search(params[:search_param])
-    
+
     if @users
       @users = current_user.except_current_user(@users)
       render partial: "friends/lookup"
@@ -18,15 +18,20 @@ class UsersController < ApplicationController
       render status: :not_found, nothing: true
     end
   end
-  
+
   def add_friend
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
-    
+
     if current_user.save
       redirect_to my_friends_path, notice: "Friend was successfully added"
     else
-      redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend" 
+      redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
 end
